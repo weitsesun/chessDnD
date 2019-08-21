9c147f31a8f8d0064189a36afbeaefe439344a97
Wayne Sun <wtse.sun@gmail.com>
2019-08-21 14:00:40 -0700

    implemented drop function on board

--- src/Board.js
+++ src/Board.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import Square from './square';
+// import Square from './square';
 import Knight from './knight';
 import { canMoveKnight, moveKnight } from './Game'
 
 import { DndProvider } from 'react-dnd';
 import HTML5Backend from 'react-dnd-html5-backend';
+
+import BoardSquare from './boardSquare';
 
 function Board({knightPosition}) {
 
@@ -27,21 +29,37 @@ function Board({knightPosition}) {
   )
 }
 
-function renderSquare(i, [knightX, knightY]) {
+function renderSquare(i, knightPosition) {
   const x = i % 8;
   const y = Math.floor(i / 8);
 
-  const black = (x + y) % 2 === 1;
-  const isKnightHere = knightX === x && knightY === y;
-  const piece = isKnightHere ? <Knight /> : null;
+  // const black = (x + y) % 2 === 1;
+  // const isKnightHere = knightX === x && knightY === y;
+  // const piece = isKnightHere ? <Knight /> : null;
+
+  // return (
+  //   <DndProvider backend={HTML5Backend}>
+  //   <div onClick={() => handleSquareClick(x, y)} key = {i} style={{width: '12.5%', height: '12.5%'}}>
+  //     <Square black = { black }>{piece}</Square>
+  //   </div>
+  //   </DndProvider>
+  // )
 
   return (
     <DndProvider backend={HTML5Backend}>
-    <div onClick={() => handleSquareClick(x, y)} key = {i} style={{width: '12.5%', height: '12.5%'}}>
-      <Square black = { black }>{piece}</Square>
+    <div key={i} style={{width: '12.5%', height: '12.5%'}}>
+      <BoardSquare x={x} y={y}>
+        {renderPiece(x, y, knightPosition)}
+      </BoardSquare>
     </div>
     </DndProvider>
   )
+}
+
+function renderPiece(x, y, [knightX, knightY]) {
+  if(x === knightX && y === knightY) {
+    return <Knight />
+  }
 }
 
 function handleSquareClick(toX, toY) {
---
+++ src/boardSquare.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import Square from './square'
+import { canMoveKnight, moveKnight } from './Game'
+import { ItemTypes } from './Constants'
+import { useDrop } from 'react-dnd'
+
+function BoardSquare({ x, y, children }) {
+  const black = (x + y) % 2 === 1
+	const [{ isOver }, drop] = useDrop({
+		accept: ItemTypes.KNIGHT,
+    drop: () => moveKnight(x, y),
+    canDrop: () => canMoveKnight(x, y),
+		collect: monitor => ({
+			isOver: !!monitor.isOver(),
+		}),
+	})
+
+  return (
+    <div
+      ref={drop}
+      style={{
+        position: 'relative',
+        width: '100%',
+        height: '100%',
+      }}
+    >
+      <Square black={black}>{children}</Square>
+      {isOver && (
+        <div
+          style={{
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            height: '100%',
+            width: '100%',
+            zIndex: 1,
+            opacity: 0.5,
+            backgroundColor: 'yellow',
+          }}
+        />
+      )}
+    </div>
+  )
+}
+
+export default BoardSquare
